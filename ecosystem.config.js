291983a037ee4007f80060f919de411ad2d5ea1e
Sefinek <contact@sefinek.net>
2026-02-05 18:53:06 +0100

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,9 +6,9 @@ module.exports = {
 		// Logging configuration
 		log_date_format: 'HH:mm:ss.SSS DD.MM.YYYY',
 		merge_logs: true,
-		log_file: '~/log/suricata-abuseipdb/combined.log',
-		out_file: '~/log/suricata-abuseipdb/out.log',
-		error_file: '~/log/suricata-abuseipdb/error.log',
+		log_file: '~/logs/suricata-abuseipdb/combined.log',
+		out_file: '~/logs/suricata-abuseipdb/out.log',
+		error_file: '~/logs/suricata-abuseipdb/error.log',
 
 		// Application restart policy settings
 		wait_ready: true,
